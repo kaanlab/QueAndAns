35cfc6b067d13a97de42ff66e33818be423091b1
kaanlab <kaan@live.ru>
2021-08-11 12:21:56 +0300

    Initial commit

---
+++ QueAndAns/Models/SimpleQuestion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueAndAns.Models
+{
+    public class SimpleQuestion
+    {
+        public string SelectedAnswer { get; set; }
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string RightIs { get; set; }
+        public int QuesId { get; set; }
+    }
+    public class MediumQuestion
+    {
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public List<string> RightAre { get; set; }
+        public List<string> SelectedAre { get; set; }
+        public int QuesId { get; set; }
+    }
+}
---
+++ QueAndAns/Models/TestInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueAndAns.Models
+{
+    public class TestInformation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<SimpleQuestion> SimpleQuestions { get; set; }
+    }
+}
